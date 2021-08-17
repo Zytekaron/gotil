ec15fb842a139ead6699601d776de8f0549039dd
Zytekaron <zytekaron@gmail.com>
2021-08-17 00:38:44 -0700

    add json methods
    - add json methods
    - documentation changes
    - leanup

--- fn/gobs.go
+++ fn/gobs.go
@@ -2,20 +2,33 @@ package fn
 
 import (
 	"encoding/gob"
+	"encoding/json"
 	"io"
 	"os"
 )
 
+// ReadJson reads a Go Object from io.Reader
+func ReadJson(r io.Reader, i interface{}) error {
+	return json.NewDecoder(r).Decode(i)
+}
+
+// ReadJsonFile reads a Go Object from a file
+func ReadJsonFile(path string, i interface{}) error {
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return ReadJson(file, i)
+}
+
 // ReadGob reads a Go Object from io.Reader
-// Unexported properties on structs will be lost
 func ReadGob(r io.Reader, i interface{}) error {
-	decoder := gob.NewDecoder(r)
-	err := decoder.Decode(i)
-	return err
+	return gob.NewDecoder(r).Decode(i)
 }
 
 // ReadGobFile reads a Go Object from a file
-// Unexported properties on structs will be lost
 func ReadGobFile(path string, i interface{}) error {
 	file, err := os.Open(path)
 	if err != nil {
@@ -26,16 +39,28 @@ func ReadGobFile(path string, i interface{}) error {
 	return ReadGob(file, i)
 }
 
+// WriteJson writes a Go Object to io.Writer
+func WriteJson(w io.Writer, obj interface{}) error {
+	return json.NewEncoder(w).Encode(obj)
+}
+
+// WriteJsonFile writes a Go Object to a file
+func WriteJsonFile(path string, obj interface{}) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return WriteJson(file, obj)
+}
+
 // WriteGob writes a Go Object to io.Writer
-// Unexported properties on structs will be lost
 func WriteGob(w io.Writer, obj interface{}) error {
-	encoder := gob.NewEncoder(w)
-	err := encoder.Encode(obj)
-	return err
+	return gob.NewEncoder(w).Encode(obj)
 }
 
 // WriteGobFile writes a Go Object to a file
-// Unexported properties on structs will be lost
 func WriteGobFile(path string, obj interface{}) error {
 	file, err := os.Create(path)
 	if err != nil {
